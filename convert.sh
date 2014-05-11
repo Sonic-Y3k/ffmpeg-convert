8b1d68543fc7f6b89134ed36741a03c5ed866803
Sonic-Y3k <sonic.y3k@googlemail.com>
2014-05-11 20:23:59 +0200

    Fixed Autocrop

--- convert.sh
+++ convert.sh
@@ -330,52 +330,7 @@ function checkSize {
 
 function cropDetect
 {
-        FFOUT=/tmp/ffout
-        CROP="1"
-        TOTAL_LOOPS="10"
- 
-        A=0
-        while [ "$A" -lt "$TOTAL_LOOPS" ] ; do
-        A="$(( $A + 1 ))"
-        SKIP_SECS="$(( 120 * $A ))"
- 
-        ffmpeg -ss $SKIP_SECS -i "$@" -vframes 20 -an -sn -vf \
-        cropdetect=30:2 -y /tmp/crop.mp4 2>$FFOUT
-        CROP[$A]=$(grep -m 1 crop= $FFOUT |awk -F "=" '{print $2}')
- 
-        done
-       
- 
-        B=0
-        while [ "$B" -lt "$TOTAL_LOOPS" ] ; do
-        B="$(( $B + 1 ))"
- 
-        C=0
-        while [ "$C" -lt "$TOTAL_LOOPS" ] ; do
-        C="$(( $C + 1 ))"
- 
-        if [ "${CROP[$B]}" == "${CROP[$C]}" ] ; then
-                COUNT_CROP[$B]="$(( ${COUNT_CROP[$B]} + 1 ))"
-        fi
-        done  
-        done
- 
-        HIGHEST_COUNT=0
- 
-        D=0
-        while [ "$D" -lt "$TOTAL_LOOPS" ] ; do
-        D="$(( $D + 1 ))"
- 
-        if [ "${COUNT_CROP[$D]}" -gt "$HIGHEST_COUNT" ] ; then
-                HIGHEST_COUNT="${COUNT_CROP[$D]}"
-                GREATEST="$D"
-        fi
-        done
-        rm /tmp/crop.mp4
-        CROP="${CROP[$GREATEST]}"
-        CROP=$(echo $CROP|awk -F ":" '{print $1-4":"$2-4":"$3+2":"$4+2}')
-        echo $CROP
-        rm $FFOUT
+ 		echo $(ffmpeg -i "$@" -t 1 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1|sed 's/crop=//g' )
 }
 
 function startEncode {
