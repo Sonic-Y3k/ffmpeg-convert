bbe8a8b736e7d92206ccde61909c725b9d0956aa
Sonic-Y3k <sonic.y3k@googlemail.com>
2014-05-11 15:18:00 +0200

    Added Autocropping

--- convert.sh
+++ convert.sh
@@ -326,6 +326,57 @@ function checkSize {
 	fi
 }
 
+
+function cropDetect
+{
+        FFOUT=/tmp/ffout
+        CROP="1"
+        TOTAL_LOOPS="10"
+ 
+        A=0
+        while [ "$A" -lt "$TOTAL_LOOPS" ] ; do
+        A="$(( $A + 1 ))"
+        SKIP_SECS="$(( 120 * $A ))"
+ 
+        ffmpeg -ss $SKIP_SECS -i "$@" -vframes 20 -an -sn -vf \
+        cropdetect=30:2 -y /tmp/crop.mp4 2>$FFOUT
+        CROP[$A]=$(grep -m 1 crop= $FFOUT |awk -F "=" '{print $2}')
+ 
+        done
+       
+ 
+        B=0
+        while [ "$B" -lt "$TOTAL_LOOPS" ] ; do
+        B="$(( $B + 1 ))"
+ 
+        C=0
+        while [ "$C" -lt "$TOTAL_LOOPS" ] ; do
+        C="$(( $C + 1 ))"
+ 
+        if [ "${CROP[$B]}" == "${CROP[$C]}" ] ; then
+                COUNT_CROP[$B]="$(( ${COUNT_CROP[$B]} + 1 ))"
+        fi
+        done  
+        done
+ 
+        HIGHEST_COUNT=0
+ 
+        D=0
+        while [ "$D" -lt "$TOTAL_LOOPS" ] ; do
+        D="$(( $D + 1 ))"
+ 
+        if [ "${COUNT_CROP[$D]}" -gt "$HIGHEST_COUNT" ] ; then
+                HIGHEST_COUNT="${COUNT_CROP[$D]}"
+                GREATEST="$D"
+        fi
+        done
+        rm /tmp/crop.mp4
+        CROP="${CROP[$GREATEST]}"
+        CROP=$(echo $CROP|awk -F ":" '{print $1-4":"$2-4":"$3+2":"$4+2}')
+        echo $CROP
+        rm $FFOUT
+}
+
 function startEncode {
 	DEFAULT_PATH="$1"
 	checkSize "$1"
@@ -334,7 +385,9 @@ function startEncode {
 	fullfile=`basename "$1"`
 	filename="${fullfile%.*}"
 	
-	nice -n 15 ffmpeg -y -vstats_file /tmp/vstats -i "$1" $(checkFileCodecs) "$dictPath/output/$filename.$DEFAULT_OUTPUTF" 2>/dev/null &                       # CHANGE THIS FOR YOUR FFMPEG COMMAND.
+	cropFrame=$(cropDetect "$1")
+	
+	nice -n 15 ffmpeg -y -vstats_file /tmp/vstats -i "$1" $(checkFileCodecs) -filter:v crop=$cropFrame "$dictPath/output/$filename.$DEFAULT_OUTPUTF" 2>/dev/null & 
         PID=$! && 
 	showFrame "$PID"
 
