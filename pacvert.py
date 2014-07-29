9b18df8bc56b54043e009625be61d123c8c54466
Sonic-Y3k <sonic.y3k@googlemail.com>
2014-07-29 18:04:07 +0200

    Update Functionallity

--- pacvert.py
+++ pacvert.py
@@ -34,7 +34,7 @@ import urllib2 # Check for new versions from the repo
 # Global Variables in all caps #
 ################################
 
-VERSION = 1.9;
+VERSION = 1.8;
 
 # Console colors
 W  = '\033[0m'  # white (normal)
@@ -83,18 +83,20 @@ def get_remote_version():
 	except IOError:
 		return -1
 		
+	
+	
 	#Get the version
-	start	= page.find("VERSION = ")
+	start	= page.find("VERSION")
 	if start != -1:
-		start	+= 10
-		rev		= page[start:";\n"]
+		page	= page[start+10:len(page)]
+		page	= page[0:page.find(";")]
 		try:
-			iver= int(rev)
+			rver= float(page)
 		except ValueError:
-			rev=rev.split('\n')[0]
-			print R+"[+] invalid version number: '"+rev+"'"
+			rver=page.split('\n')[0]
+			print R+" [!] invalid version number: '"+page+"'"
 			
-	return iver
+	return rver
 
 def upgrade():
 	"""
@@ -104,8 +106,66 @@ def upgrade():
 	global VERSION
 	try:
 		print GR+" [!]"+W+" upgrading requires an "+R+"internet connection"+W
-		print GR+" [+]"+W+" checking for latest version..."
+		print GR+" [+]"+W+" checking for latest version..."+W
 		remote_version = get_remote_version()
+		
+		if remote_version == -1:
+			print R+" [!]"+O+" unable to access github"+W
+		elif remote_version > float(VERSION):
+			print GR+" [!]"+W+" version "+G+str(remote_version)+W+" is "+G+"available!"+W
+			response = raw_input(GR+" [+]"+W+" do you want to upgrade to the latest version? (y/n): ")
+			if not response.lower().startswith("y"):
+				print GR+" [-]"+W+" upgrading "+O+"aborted"+W
+				return
+			
+			#Download script and replace this one
+			print GR+" [+] "+G+"downloading"+W+" update..."
+			try:
+				sock = urllib2.urlopen("https://raw.githubusercontent.com/Sonic-Y3k/ffmpeg-convert/master/pacvert.py")
+				page = sock.read()
+			except IOError:
+				page = ''
+			
+			if page == '':
+				print R+' [+] '+O+'unable to download latest version'+W
+				exit_gracefully(1)
+				
+			# Create/save the new script
+			f=open('pacvert_new.py','w')
+			f.write(page)
+			f.close()
+			
+			# The filename of the running script
+			this_file = __file__
+			if this_file.startswith('./'):
+				this_file = this_file[2:]
+			
+			# create/save a shell script that replaces this script with the new one
+			f = open('update_pacvert.sh','w')
+			f.write('''#!/bin/sh\n
+				rm -rf ''' + this_file + '''\n
+				mv pacvert_new.py ''' + this_file + '''\n
+				rm -rf update_pacvert.sh\n
+				chmod +x ''' + this_file + '''\n
+				''')
+			f.close()
+			
+			# Change permissions on the script
+			returncode = call(['chmod','+x','update_pacvert.sh'])
+			if returncode != 0:
+				print R+' [!]'+O+' permission change returned unexpected code: '+str(returncode)+W
+				exit_gracefully(1)
+			
+			# Run the script
+			returncode = call(['sh','update_pacvert.sh'])
+			if returncode != 0:
+				print R+' [!]'+O+' upgrade script returned unexpected code: '+str(returncode)+W
+				exit_gracefully(1)
+			
+			print GR+' [+] '+G+'updated!'+W+' type "./' + this_file + '" to run again'
+		else:
+			print GR+' [-]'+W+' your copy of wifite is '+G+'up to date'+W
+		
 	except KeyboardInterrupt:
 		print R+'\n (^C)'+O+' Pacvert upgrade interrupted'+W
 		exit_gracefully(0)
